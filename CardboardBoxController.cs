04139c4d1c44541f4ceba9a43a682aea40dd6305
aliuqianbo <qianbo94@gmail.com>
2023-10-14 20:58:48 +0800

    Add files via upload

---
+++ CardboardBoxController.cs
@@ -0,0 +1,157 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardboardBoxController : MonoBehaviour
+{
+    [SerializeField] private GameObject grabedEffect;
+    [SerializeField] private bool horizontalMovementOnly;
+    [SerializeField] private bool verticalMovementOnly;
+    private PlayerController playerController;
+    private GameInput gameInput;
+    private BoxCollider boxCollider;
+    private bool isGrabed;
+    private float speed;
+    public float dropSpeed;
+    [SerializeField] private float grabSpeedModifier;
+
+    private SoundManager soundManager;
+    private AudioSource _cachedAudioSource;
+
+    public bool isResearchMachine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGrabed = false;
+        boxCollider = transform.GetComponent<BoxCollider>();
+
+        GameObject player = GameObject.Find("Player_TVguy");
+        GameObject inputObject = GameObject.Find("GameInput");
+        playerController = player.GetComponent<PlayerController>();
+        gameInput = inputObject.GetComponent<GameInput>();
+
+        var soundManagerArray = FindObjectsOfType<SoundManager>();
+        soundManager = soundManagerArray[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //handle box drop
+        Vector3 dropDir = new Vector3(0, -1f, 0);
+        float dropDistance = dropSpeed * Time.deltaTime;
+        Vector3 boxCenter = transform.position + new Vector3(0, 1.05f, 0);
+        Vector3 boxHalfExt = new Vector3(boxCollider.size.x / 2, boxCollider.size.y / 2, boxCollider.size.z / 2); 
+        bool canDrop = !Physics.BoxCast(boxCenter, boxHalfExt, dropDir, transform.rotation, dropDistance, 1 << 0);
+        if (canDrop)
+        {
+            transform.position += dropDir * dropSpeed * Time.deltaTime;
+            if (transform.position.y < 0)
+            {
+                transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+            }
+        }
+
+        //handle player grab box movement
+        if (isGrabed)
+        {
+            UpdatePlayerMoveStat();
+            //get Player movement input
+            Vector2 inputVector = gameInput.getMovementVectorNormalized();
+
+            //disable switch
+            if (playerController.GetDisableInputStatus())
+            {
+                inputVector *= Vector2.zero;
+            }
+
+            //play audio
+            if (inputVector != Vector2.zero)
+            {
+                if (!isResearchMachine)
+                {
+                    _cachedAudioSource = soundManager.PlaySoundAndReturnSource(soundManager.boxSlide, 0.1f);
+                }
+                else
+                {
+                    _cachedAudioSource = soundManager.PlaySoundAndReturnSource(soundManager.metalSlide, 0.1f);
+                }
+            }
+            else
+            {
+                _cachedAudioSource.Stop();
+            }
+
+            //horizontal movement only
+            if (horizontalMovementOnly == true)
+            {
+                inputVector.x = -inputVector.y;
+                boxHalfExt = new Vector3(boxCollider.size.y / 2, boxCollider.size.y / 2, boxCollider.size.y / 2);
+            }
+
+            //vertical movement only
+            if (verticalMovementOnly == true)
+            {
+                inputVector.y = inputVector.x;
+                boxHalfExt = new Vector3(boxCollider.size.y / 2, boxCollider.size.y / 2, boxCollider.size.y / 2);
+            }
+
+
+
+            Vector3 moveDir = new Vector3(inputVector.x, 0.0f, inputVector.y);
+            float moveDistance = speed  * Time.deltaTime;
+
+            bool canMove = !Physics.BoxCast(boxCenter, boxHalfExt, moveDir, transform.rotation, moveDistance, 1 << 0);
+
+            if (!canDrop && canMove && playerController.getCanUseItemStatus())
+            {
+                transform.position += moveDir * speed * Time.deltaTime;
+            }
+
+            //when too far break grab
+            float _HorDistance;
+            _HorDistance = Vector3.Distance(transform.position, playerController.transform.position);
+            //Debug.Log("_HorDistance:" + _HorDistance);
+            if (_HorDistance > 2)
+            {
+                isGrabed = false;
+                grabedEffect.SetActive(false);
+                playerController.isGrabbing = false;
+            }
+        }
+
+
+
+		//when too high or low , break grab
+		float _VerDistance;
+		_VerDistance = transform.position.y - playerController.transform.position.y;
+		_VerDistance = System.Math.Abs(_VerDistance);
+        //Debug.Log("_VerDistance" + _VerDistance);
+
+    }
+
+    private void UpdatePlayerMoveStat()
+    {
+        speed = playerController.GetPlayerGrabMoveSpeed();
+        dropSpeed = playerController.GetPlayerDropSpeed();
+    }
+
+    public void toggleGrabStatus()
+    {
+        isGrabed = !isGrabed;
+        if (isGrabed == false)
+        {
+            grabedEffect.SetActive(false);
+        }
+        else
+        {
+            grabedEffect.SetActive(true);
+        }
+    }
+
+    public bool getGrabStatus()
+    {
+        return isGrabed;
+    }
+}
---
+++ FixedSentryGun.cs
@@ -0,0 +1,155 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedSentryGun : MonoBehaviour
+{
+    [SerializeField] private BoxAreaScan boxAreaScan;
+    [SerializeField] private Transform bulletSpawnPoint;
+    [SerializeField] private Animator BarrelAnimator;
+    [SerializeField] private ParticleSystem muzzleFlare;
+    [SerializeField] private ParticleSystem bulletImpact;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private GameObject electricEffect;
+    [SerializeField] private AudioSource audioSource_shoot;
+    [SerializeField] private AudioSource audioSource_hit;
+    [SerializeField] private GameObject audioSource_broken;
+
+    private bool addBulleSpread = true;
+    private Vector3 bulletSpreadVariance = new Vector3(0.05f, 0f, 0.05f);
+    private PlayerStats playerStats;
+    private StateManager_Entity stateManager_Entity;
+    private float SHOOT_INTERVAL = 0.25f;
+    private float lastShootTime = 0;
+    private int BULLET_DAMAGE = 20;
+
+
+    private float brokenTime = 999999f;
+    private float timer = 0;
+    [HideInInspector] public bool isBroken = false;
+
+    private SoundManager soundManager;
+
+    private void Start()
+	{
+        var soundManagerArray = FindObjectsOfType<SoundManager>();
+        soundManager = soundManagerArray[0];
+    }
+
+	// Update is called once per frame
+	void Update()
+    {
+
+        if (boxAreaScan.counter > 0)
+        {
+            if (!boxAreaScan.coveredBySmoke && !boxAreaScan.blockedByWall && !isBroken)
+            {
+                Shoot();
+            }         
+        }
+
+        if (isBroken)
+        {
+            electricEffect.SetActive(true);
+            timer += Time.deltaTime;
+
+            //play audio
+            audioSource_broken.SetActive(true);
+
+            if (timer >= brokenTime)
+            {
+                electricEffect.SetActive(false);
+                timer = 0;
+                isBroken = false;
+
+                //stop audio
+                audioSource_broken.SetActive(true);
+            }
+            
+        }
+    }
+
+    private void Shoot()
+    {
+        if ((lastShootTime + SHOOT_INTERVAL) < Time.time)
+        {
+            Debug.Log("shooting!!!!!");
+            lastShootTime = Time.time;
+            //play animation
+            BarrelAnimator.Play("Fire");
+            //play effect
+            muzzleFlare.Play();
+            //play audio
+            soundManager.PlaySoundWithTargetAudioSource(audioSource_shoot, soundManager.shootBullet, 1f);
+
+            //doing Raycast, dealing damage
+
+            Vector3 direction = GetDirection();
+
+            if (Physics.Raycast(bulletSpawnPoint.position, direction, out RaycastHit hit, 21.6f))
+            {
+
+                GameObject actualBullet = Instantiate(bullet, bulletSpawnPoint.position, transform.rotation);
+                StartCoroutine(MoveBullet(actualBullet, hit));
+
+                if (hit.transform.tag == ("Player"))
+                {
+                    Debug.Log("Hit Player");
+                    playerStats = hit.transform.GetComponent<PlayerStats>();
+                    playerStats.PlayerTakingHPDamage(BULLET_DAMAGE);
+
+                    //play audio
+                    soundManager.PlaySoundWithTargetAudioSource(audioSource_hit, soundManager.bulletHit, 1f);
+                }
+                else if (hit.transform.tag == ("Entity"))
+                {
+                    Debug.Log("Hit Entity!");
+                    stateManager_Entity = hit.transform.GetComponent<StateManager_Entity>();
+                    stateManager_Entity.currentState = stateManager_Entity.STATE_HURT;
+
+                    //play audio
+                    soundManager.PlaySoundWithTargetAudioSource(audioSource_hit, soundManager.bulletHit, 1f);
+                }
+                
+            }
+            
+        }
+    }
+
+    private Vector3 GetDirection()
+    {
+        Vector3 direction = transform.forward;
+        if (addBulleSpread)
+        {
+            direction += new Vector3(
+                Random.Range(-bulletSpreadVariance.x, bulletSpreadVariance.x),
+                Random.Range(-bulletSpreadVariance.y, bulletSpreadVariance.y),
+                Random.Range(-bulletSpreadVariance.z, bulletSpreadVariance.z)
+            );
+
+            direction.Normalize();
+        }
+
+        return direction;
+    }
+
+    private IEnumerator MoveBullet(GameObject bullet, RaycastHit hit)
+    {
+        float time = 0;
+        Vector3 startPosition = bullet.transform.position;
+
+        while (time < 1)
+        {
+            bullet.transform.position = Vector3.Lerp(startPosition, hit.point, time);
+            time += Time.deltaTime / 0.05f;
+
+            yield return null;
+        }
+
+        bullet.transform.position = hit.point;
+        Instantiate(bulletImpact, hit.point, Quaternion.LookRotation(hit.normal));
+
+        Destroy(bullet, 0.05f);
+
+    }
+}
